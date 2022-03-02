f2cdddebec12784d6e9f180a93f5f5a6d1b8bb09
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-03-03 00:19:32 +0600

    Weather status updated

--- js/app.js
+++ js/app.js
@@ -3,9 +3,24 @@ const searchTemperature = () => {
     const input = document.getElementById('city-name');
     const inputValue = input.value;
     input.value = "";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${API_KEY}`;
-    console.log(url)
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then(res => res.json())
-    .then(data = console.log(data))
+    .then(data => displayWeather(data))
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+const displayWeather = weather => {
+    // set weather updates
+    setInnerText('city', weather?.name ? weather?.name : "City not found !!");
+    setInnerText('temperature', weather.main.temp);
+    setInnerText('condition', weather.weather[0].main)
+    // set weather icon
+    const urlIcon = ` http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+    const imageIcon = document.getElementById('weather-icon');
+    imageIcon.setAttribute('src', urlIcon);
+    
+
 }
