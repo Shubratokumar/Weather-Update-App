f51920125c86830995014b66991dbe0b26b4e8bb
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-03-04 16:56:28 +0600

    country code and cloud description added

--- js/app.js
+++ js/app.js
@@ -15,12 +15,14 @@ const setInnerText = (id, text) => {
 const displayWeather = weather => {
     // set weather updates
     setInnerText('city', weather?.name ? weather?.name : "City not found !!");
-    setInnerText('temperature', weather.main.temp);
-    setInnerText('condition', weather.weather[0].main)
+    setInnerText('countrycode', weather?.sys.country ? weather?.sys.country : "null");
+    setInnerText('temperature', weather?.main.temp ? weather?.main.temp : " ");
+    setInnerText('condition', weather?.weather[0].main ? weather?.weather[0].main : " ");
+    setInnerText('clouddescription', weather?.weather[0].description ? weather?.weather[0].description : " ");
+
+
     // set weather icon
-    const urlIcon = ` http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+    const urlIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
     const imageIcon = document.getElementById('weather-icon');
     imageIcon.setAttribute('src', urlIcon);
-    
-
 }
